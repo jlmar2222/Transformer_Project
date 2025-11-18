969a36dd9aa1733ae24c15462460cfd882634ed8
jlmar2222 <jlmar2222@gmail.com>
2025-11-18 13:29:39 +0100

    Adding Multihead  Attention,FNN,  LayerNormalization and Drop Out to cervantino.py

--- cervantino.py
+++ cervantino.py
@@ -6,14 +6,18 @@ from torch.nn import functional as F
 
 
 # Hypermarameters
-batch_size = 32
-block_size = 8 # COntext Window
-max_epoch = 3000
-eval_interval = 300
-lerning_rate = 1e-2
+batch_size = 4
+block_size = 8 # Context Window
+max_epoch = 5000
+eval_interval = 500
+lerning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
+heads_num = 4
+head_size = n_embd // heads_num
+dropout = 0.2
+n_layers = 6 
 #---------------------
 
 
@@ -81,7 +85,7 @@ def estimate_loss():
 
 class Head(nn.Module):
   
-  def __init__(self,head_size):
+  def __init__(self):
     super().__init__()
 
     self.key = nn.Linear(n_embd, head_size, bias=False)
@@ -90,6 +94,8 @@ class Head(nn.Module):
     #ahora añadimos un buffer que se llama que es como una variable (matriz triangular)
     #un buffer es algo de la arquitectura pero que no es un Parametro entrenable
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    self.drop = nn.Dropout(dropout)
 
   def forward(self, x):
     B,T,C = x.shape
@@ -101,21 +107,70 @@ class Head(nn.Module):
     QK = q@k.transpose(-2,-1) * C**(-0.5)#(B,T,head_size) @ (B,head_size,T)
     QK = QK.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
     QK = F.softmax(QK, dim=-1)
+    QK = self.drop(QK)
 
     output = QK @ v  # (B, T, T) @ (B,T,head_size)
 
     return output
   
 
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self):
+       super().__init__()
+       self.heads = nn.ModuleList([Head() for _ in range(heads_num)])
+       self.proj = nn.Linear(n_embd, n_embd) # Añadimo sprojección para mejorar el entreno
+       self.drop = nn.Dropout(dropout)
+    
+    def forward(self, x):
+       out = torch.cat([h(x) for h in self.heads], dim=-1)   
+       out = self.proj(out)
+       out = self.drop(out)
+       return out 
+  
+
+class FeedForward(nn.Module):
+   def __init__(self):
+      super().__init__()
+      self.net = nn.Sequential(
+         nn.Linear(n_embd,4*n_embd),
+         nn.ReLU(),
+         nn.Linear(4*n_embd, n_embd),
+         nn.Dropout(dropout)
+      )
+
+   def forward(self,x):
+      return self.net(x)
+
+
+class Block(nn.Module):
+   def __init__(self):
+      super().__init__()
+      self.sa_mhead = MultiHeadAttention()
+      self.ffn = FeedForward()
+      self.lnorm1 = nn.LayerNorm(n_embd)
+      self.lnorm2 = nn.LayerNorm(n_embd)
+    
+   def forward(self, x):     
+      # Residual block / short-cut 
+      x = x + self.sa_mhead(self.lnorm1(x))  #(Comunicación); normalizamos x antes de entrar en bloque
+      # Residual block / short-cut
+      x = x + self.ffn(self.lnorm2(x)) # (Computación); normalizamos x antes de entrar en bloque
+     
+      return x
+    
+
+
 
 class BigramModel(nn.Module):
-  def __init__(self, vocab_size):
+  def __init__(self):
     super().__init__()
     # la matriz de pesos que el modelo simple aprenderá
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # ahora esto no nos da directamente los logits
     self.pos_embedding_table = nn.Embedding(block_size,n_embd)
-    self.sa_head = Head(n_embd)
-    self.lm_head = nn.Linear(n_embd,vocab_size)
+    self.blocks = nn.Sequential(*[Block() for _ in range(n_layers)])  # *[Block() for _ in range(n_layers)] --> nn.Sequential (Block(),Block(),...,Block())
+    self.lnorm = nn.LayerNorm(n_embd)
+    self.lm_head = nn.Linear(n_embd,vocab_size)    
 
   def forward(self, idx, targets=None):
       
@@ -126,8 +181,8 @@ class BigramModel(nn.Module):
                                         # torch.arrenge(T), generates a tensor with de positions [0,1,...,T]
       
       x = tok_emb + pos_emb
-      x = self.sa_head(x) # Pasamos el token por un bloque de atención
-
+      x = self.blocks(x)
+      x = self.lnorm(x)
       logits = self.lm_head(x) # (B,T,vocab_size)
 
       if targets is None:
@@ -141,7 +196,7 @@ class BigramModel(nn.Module):
       return logits, loss
 
   def generate(self, idx, max_new_tokens):
-        # 
+         
         for _ in range(max_new_tokens):
             # adjust lenght of the idx to match max_size of block_size
             idx_cond = idx[:,-block_size:]
@@ -161,7 +216,7 @@ class BigramModel(nn.Module):
 
 #---------------------------
 
-model = BigramModel(vocab_size)
+model = BigramModel()
 m = model.to(device)
 
 # set the PyTorch optimizer 
@@ -192,4 +247,4 @@ for epoch in range(max_epoch):
 # finall generate results:
 
 user_input = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(user_input, max_new_tokens=100)[0].tolist()))
+print(decode(m.generate(user_input, max_new_tokens=1000)[0].tolist()))
